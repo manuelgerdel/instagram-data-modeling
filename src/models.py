e959d9258ad5cd768aba278a4286a414d1433bd9
Manuel Gerdel <122245905+manuelgerdel@users.noreply.github.com>
2023-08-17 17:44:59 +0000

    all project done

--- src/models.py
+++ src/models.py
@@ -7,26 +7,88 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+   #Generics
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(30), nullable = False)
+    email = Column(String(200), nullable = False)
+    name = Column(String(50), nullable = False)
+    last_name = Column(String(50), nullable = True)
+    birth_day = Column(String(8), nullable = False)
+    age = Column(Integer, nullable = True)
+    personal_description = Column(String(30), nullable = True)
 
-    def to_dict(self):
-        return {}
+    #Relations
+    posts = relationship("Post", back_populates = "user")
+    likes = relationship("Like", back_populates = "user")
+    commets = relationship("Comment", back_populates = "user")
+    stories = relationship("Story", back_populates = "user")
+    followers = relationship("Follower", back_populates = "user")
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    #Generics
+    id = Column(Integer, primary_key = True)
+    content = Column(String(5000), nullable = False)
+
+    #Relations
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False) 
+    likes = relationship("Like", back_populates = "post")
+    posts = relationship("Post", back_populates = "post")
+    commets = relationship("Comment", back_populates = "user")
+
+
+class Like(Base):
+    __tablename__ = 'like'
+
+    #Generics
+    id = Column(Integer, primary_key = True)
+
+    #Relations
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    post_id = Column(Integer, ForeignKey("post.id"), nullable=False) 
+    comment_id = Column(Integer, ForeignKey("comment.id"), nullable=False)
+    story_id = Column(Integer, ForeignKey("story.id"), nullable=False) 
+
+class Comment(Base):
+    __tablename__ = 'comment'
+
+    #Generics
+    id = Column(Integer, primary_key = True)
+
+    #Relations
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    post_id = Column(Integer, ForeignKey("post.id"), nullable=False) 
+    comment_id = Column(Integer, ForeignKey("comment.id"), nullable=False)
+    likes = relationship("Like", back_populates = "comment")
+
+
+class Story(Base):
+    __tablename__ = 'story'
+
+    #Generics
+    id = Column(Integer, primary_key = True)
+    content = Column(String(30), nullable = False)
+    message = Column(String(30), nullable = True)
+
+    #Relations
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    post_id = Column(Integer, ForeignKey("post.id"), nullable=False)
+    likes = relationship("Like", back_populates = "story")
+
+class Follower(Base):
+    __tablename__ = 'follower'
+
+    #Generics
+    id = Column(Integer, primary_key = True)
+
+    #Relations
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+
+
 
 ## Draw from SQLAlchemy base
 try:
